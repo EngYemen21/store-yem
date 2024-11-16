1bb714cd330385415ab2b3f9e009d3704a363a27
mohammed bandar <104012992+EngYemen21@users.noreply.github.com>
2024-11-17 00:41:04 +0300

    Update Product.jsx

--- src/Component/Product.jsx
+++ src/Component/Product.jsx
@@ -1,126 +1,126 @@
-import React ,{useEffect ,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Skeleton from "react-loading-skeleton";
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addItem } from '../redux/Action';
-import { useParams } from 'react-router'
-import { NavLink } from 'react-router-dom';
-import toast, { Toaster } from 'react-hot-toast';
+import { useParams, NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Product() {
-    const {id}= useParams();
-    const [product,setProduct] =useState([]);
-    const [loading , setloading]=useState(false);
-    const dispatch =useDispatch();
-  
-    const addProduct= (product)=>{
-        dispatch(addItem(product));
+    const { id } = useParams();
+    const [product, setProduct] = useState([]);
+    const [user_id, setUserId] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+      const [data, setData] = useState([]);
+   
 
 
-        toast('Done ..!  added to cart', {
-            duration: 4000,
-            position: 'top-center',
-          
-            // Styling
-            style: { background:'#142A74',
-            color: "#FFF",
-            width:'250px',
-            height:'50px',
+      useEffect(() => {
+        const getUser = async () => {
+       
+            try {
+                const responseUser = await fetch(`http://127.0.0.1:8000/api/getUserId`);
+                const dataUser = await responseUser.json();
+                setUserId(dataUser);
+            } catch (error) {
+                console.error('Error fetching product:', error);
+            }
+       
+        }
+        getUser();
+    }, []);
+
+    useEffect(() => {
+        const getProduct = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch(`https://fakestoreapi.in/api/products/${id}`).then(res => res.json());
+                // const data = await response.json();
+
+                // setData(response.products);
+                console.log(response.product)
+                // setfilter(response.products);
+                // setloading(false);
 
 
-        
-        },
-            className: '',
-          
-            // Custom Icon
-            icon: '👏',
-          
-            // Change colors of success/error/loading icon
-            iconTheme: {
-              primary: '#000',
-              secondary: '#fff',
-            },
-          
-            // Aria
-            ariaProps: {
-              role: 'status',
-              'aria-live': 'polite',
-            },
-          });
-        
-    }
-    
-    useEffect(()=>{
-        const getProduct =async ()=>{
-            setloading(true);
-            const response=await fetch(`https://fakestoreapi.com/products/${id}`);
-            setProduct(await response.json());
-           
-            setloading(false);
+                setProduct(response.product);
+            } catch (error) {
+                console.error('Error fetching product:', error);
+            }
+            setLoading(false);
         }
         getProduct();
+    }, [id]);
 
-    },[])
-    const Loading =()=>{
+    const addProduct = (product) => {
+        dispatch(addItem(product));
+        store(user_id);
+    };
+
+    const store = async (product) => {
+        try {
+            await axios.post('http://127.0.0.1:8000/api/product', {
+                title: product.title,
+                description: product.description,
+                price: product.price,
+                image: product.image,
+            }).then(({data})=>{
+                console.log(data.message)
+            });
+            console.log('Product saved successfully');
+        } catch (response) {
+           if (response.status==442){
+            console.log('Error saving product:',response.data.error);
+           } 
+           else {
+            console.log (product);
+           }
+        }
+    };
+
+    const Loading = () => {
         return (
             <>
-             <div className="col-md-6">
-        <Skeleton height={400}/>
-        </div>
-        <div className="col-md-6" style={{lineHeight:2  }}>
-        <Skeleton height={50} width={300}/>
-        <Skeleton height={75}/>
-        <Skeleton height={25} width={150}/>
-        <Skeleton height={50}/>
-        <Skeleton height={150}/>
-        <Skeleton height={50} width={100}/>
-        <Skeleton height={50} width={100} style={{ marginLeft:6 }}/>
-       
-   </div>
-            
+                {/* Skeleton loading UI */}
             </>
         )
     }
-    const ShowProduct= ()=>{
-        
+
+    const ShowProduct = () => {
         return (
-            < >
-            <div className='col-md-6 my-4'>
-                <img src={product.image} alt={product.title}  className='h-96 w-96' />
-            </div>
-            <div className='col-md-6 my-4'>
-                <h4 className='text-uppercase text-black-50 p-1'>
-                    {product.category}
-                </h4>
-                <h1 className='p-1'>{product.title}</h1>
-                <p className="lead fw-bolder text-blue-500">
-                    Reating {product.rating && product.rating.rate }
-                    <i className="fa fa-star"></i>
-                </p>
-                <h3 className="display-7 fw-bolder p-1">
-                    $ {product.price}
-                </h3>
-                <p className='load'>{product.description}</p>
-                <button className=' px-4 py-2 m-3  text-black border-1 outline-none rounded-md no-underline text-center  border-blue-950  hover:bg-blue-700' onClick={()=>addProduct(product)}>Add to cart</button>
-                <Toaster />
-                <NavLink to="/cart" className=' px-4 py-2 m-3  text-black border-1 outline-none rounded-md no-underline text-center  border-blue-900  hover:bg-blue-900'>go to cart</NavLink>
-            </div>
-            
-            
-            
+            <>
+                <div className='col-md-6 my-4'>
+                    <img src={product.image} alt={product.title} height="400px" width="400px" />
+                </div>
+                <div className='col-md-6 my-4'>
+                    <h4 className='text-uppercase text-black-50'>
+                        {product.category}
+                    </h4>
+                    <h1 className='display-5'>{product.title}</h1>
+                    <p className="lead fw-bolder">
+                        Rating: {product.rating && product.rating.rate}
+                        <i className="fa fa-star"></i>
+                    </p>
+                    <h3 className="display-6 fw-bolder my-4">
+                        $ {product.price}
+                    </h3>
+                    <p className='load'>{product.description}</p>
+                    <button className='btn btn-outline-dark px-4 py-2' onClick={() => addProduct(product)}>Add to cart</button>
+                    <NavLink to="/cart" className='btn btn-dark ms-2 px-3 py-2'>Go to cart</NavLink>
+                </div>
             </>
         )
     }
-  return (
 
-    <div>
-          <div className="container">
-        <div className="row">
-                {loading ? <Loading /> :<ShowProduct/> }
-               
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    {loading ? <Loading /> : <ShowProduct />}
                 </div>
-
-          </div>
-       
-    </div>
-  )
+            </div>
+        </div>
+    )
 }
